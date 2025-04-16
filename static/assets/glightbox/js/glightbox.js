a616e6738e33ac07a0ee39604bde15ec2cc633a6
Yaroslav Bochkov <cruelty86@yandex.ru>
2025-04-16 13:19:09 +0300

    Track all static assets with LFS

---
+++ static/assets/glightbox/js/glightbox.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0fba371ae8df71329df6aada63845f9727a11c0b9c9fda130a864bbc814fbe5c
+size 110188
---
+++ static/assets/purecounter/purecounter.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11a933100ccfe156b93199371bebe899d4239553e208c19401039756baf0a360
+size 4951
